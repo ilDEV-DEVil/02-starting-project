5ed4c5b1cd87d88a9da31889298be3843c8e296f
Nimesh <nimesh.palakuttige@globant.com>
2025-03-06 12:23:10 +0100

    fix: adds bootstrap on appmodule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from "@angular/core";
 import { AppComponent } from "./app.component";
+import { BrowserModule } from "@angular/platform-browser";
 
 @NgModule({
     declarations: [AppComponent],
-    exports: [AppComponent],
+    imports: [BrowserModule],
+    bootstrap: [AppComponent]
 })
 export class AppModule {};
