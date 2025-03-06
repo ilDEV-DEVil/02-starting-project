2d37e4254ffbc5c84bc4730ca4efc9535dfe52cd
Nimesh <nimesh.palakuttige@globant.com>
2025-03-06 12:16:10 +0100

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from "@angular/core";
+import { AppComponent } from "./app.component";
+
+@NgModule({
+    declarations: [AppComponent],
+    exports: [AppComponent],
+})
+export class AppModule {};
