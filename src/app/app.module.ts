6759b8889d333d3d21ae45cc750a3fbad4ac5193
Nimesh <nimesh.palakuttige@globant.com>
2025-03-07 11:11:31 +0100

    feat: adds user-input component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from "@angular/core";
 import { AppComponent } from "./app.component";
 import { BrowserModule } from "@angular/platform-browser";
 import { HeaderComponent } from "./components/header/header.component";
+import { UserInputComponent } from "./components/user-input/user-input.component";
 
 @NgModule({
     declarations: [
         AppComponent,
-        HeaderComponent
+        HeaderComponent,
+        UserInputComponent
     ],
     imports: [BrowserModule],
     bootstrap: [AppComponent]
